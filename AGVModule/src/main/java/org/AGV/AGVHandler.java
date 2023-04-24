8d7ab110d620c62bc186e8719e9176b4bcfb9166
Harun-Abdi <91126094+Harun-Abdi@users.noreply.github.com>
2023-04-24 14:12:42 +0200

    shit aint working

--- AGVModule/src/main/java/org/AGV/AGVHandler.java
+++ AGVModule/src/main/java/org/AGV/AGVHandler.java
@@ -1,42 +1,47 @@
 package org.AGV;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Objects;
 
-public class AGVHandler {
+
+public class AGVHandler implements HttpHandler {
 
     int idle = 1;
     int executing = 2;
     int charging = 3;
 
+    public void handle(HttpExchange httpExchange) throws IOException { //This method handles the http exchange, by checking whether it is a GET or a POST request.
+        String requestParamValue = null;
 
-    private String moveToChargerOperation() throws IOException {
+        if ("GET".equals(httpExchange.getRequestMethod())) {
+            requestParamValue = moveToChargerOperation(httpExchange);
 
-        if (httpExchange.getRequestURI().toString().contains("MoveToChargerOperation")) {
-            Write.writeCntrlCmd(Integer.parseInt(httpExchange.getRequestURI().toString().split("\\?")[1].split("=")[1]));
-
-            // efter split smid i string
-
-            String json = "MoveToChargerOperation";
-            return AGVController.handlePostRequest(json);
+        } else if ("POST".equals(httpExchange)) {
 
 
-
-
-
-        /*
-        if (URL.contains("moveToChargerOperation) {
-            insert into json = "{\"State\":1,\"Program name\":\"MoveToChargerOperation\"}"
-                    */
+        }
+        handleResponse(httpExchange, requestParamValue); //It then uses both the return value of the way we handled our request, and the http exchange, to execute the method that actually handles the response.
     }
 
-
-
-        /* Executing
-        if (URL.contains("State:2")) {
-            insert into json
-*/
+    private String moveToChargerOperation(HttpExchange httpexchange) throws IOException {
 
         return null;
     }
+
+    private void handleResponse(HttpExchange httpExchange, String requestParamValue) throws IOException {
+        String encoding = "UTF-8";
+        OutputStream outputStream = httpExchange.getResponseBody();
+        StringBuilder htmlBuilder = new StringBuilder();
+
+        //htmlBuilder.append(Objects.requireNonNullElse(requestParamValue, "Object/value returned is null"));//This checks to see if requestParamValue is null, and if it is, appends the String instead of the value.
+
+
+    }
+
+
+
 }
