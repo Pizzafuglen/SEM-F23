f41460fb3f24dd01475debb08ad7f734e7f6a68d
Christoffer Rosenkilde <Crose19@student.sdu.dk>
2023-04-24 14:10:26 +0200

    Work begun on the WHHandler.java and WHServer.java

--- WHModule/src/main/java/com/f23/javaserver/WHHandler.java
+++ WHModule/src/main/java/com/f23/javaserver/WHHandler.java
@@ -1,4 +1,51 @@
 package com.f23.javaserver;
 
-public class WHHandler {
+import com.baeldung.soap.ws.client.generated.IEmulatorService_Service;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.apache.commons.lang3.StringEscapeUtils;
+
+import javax.json.Json;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class WHHandler implements HttpHandler {
+    public IEmulatorService_Service ies = new IEmulatorService_Service();
+    private StringBuilder sb = new StringBuilder();
+    private String reVal;
+    @Override
+    public void handle(HttpExchange he) throws IOException {
+
+        switch (he.getRequestMethod()) {
+            case "GET" -> reVal = handleGet(he);
+            case "POST" -> reVal = handlePost(he);
+        }
+
+        httpResponse(he, reVal);
+    }
+
+    private String handleGet(HttpExchange he) {
+        return String.valueOf(ies.getBasicHttpBindingIEmulatorService().getInventory());
+    }
+
+    private String handlePost(HttpExchange he) {
+        return String.valueOf(ies.getBasicHttpBindingIEmulatorService().getInventory());
+    }
+
+    private void httpResponse(HttpExchange he, String reVal) throws IOException {
+        OutputStream os = he.getResponseBody();
+        sb.append(reVal);
+
+        System.out.println(ies.getBasicHttpBindingIEmulatorService().getInventory().toString());
+
+        String hr = StringEscapeUtils.escapeHtml4(sb.toString());
+
+        he.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+        he.getResponseHeaders().set("Content-Type", "text/html; charset=UTF-8");
+        he.sendResponseHeaders(200, hr.length());
+
+        os.write(hr.getBytes());
+        os.flush();
+        os.close();
+    }
 }
--- WHModule/src/main/java/com/f23/javaserver/WHServer.java
+++ WHModule/src/main/java/com/f23/javaserver/WHServer.java
@@ -1,4 +1,29 @@
 package com.f23.javaserver;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class WHServer {
+    public static void StartServer(ThreadPoolExecutor tpe) {
+        HttpServer hhs;
+
+        try {
+            hhs = HttpServer.create(new InetSocketAddress("localhost",8001),0);
+            hhs.createContext("/1/", new WHHandler());
+
+            hhs.setExecutor(tpe);
+            hhs.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void main(String[] args) {
+        ThreadPoolExecutor tpe = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+
+        WHServer.StartServer(tpe);
+    }
 }
