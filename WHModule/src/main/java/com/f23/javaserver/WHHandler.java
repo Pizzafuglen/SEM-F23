777f2505553658c391d4543835298c5ee4713b1f
Christoffer Rosenkilde <Crose19@student.sdu.dk>
2023-05-04 15:04:30 +0200

    Changes made to the WHHandler.java - Changed the way the handle method works, and reduced the amount of methods

--- WHModule/src/main/java/com/f23/javaserver/WHHandler.java
+++ WHModule/src/main/java/com/f23/javaserver/WHHandler.java
@@ -1,66 +1,33 @@
 package com.f23.javaserver;
 
-import com.baeldung.soap.ws.client.generated.IEmulatorService_Service;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import org.apache.commons.lang3.StringEscapeUtils;
-
-import javax.json.Json;
 import java.io.IOException;
 import java.io.OutputStream;
 
 public class WHHandler implements HttpHandler {
-    public IEmulatorService_Service ies = new IEmulatorService_Service();
-    private StringBuilder sb = new StringBuilder();
-    private String reVal;
     @Override
     public void handle(HttpExchange he) throws IOException {
-        System.out.println(he.getRequestMethod());
-        switch (he.getRequestMethod()) {
-            case "GET" -> reVal = handleGet(he);
-            case "POST" -> reVal = handlePost(he);
+        System.out.println(he.getRequestURI().toString().split("\\?")[1]);
+
+        String reVal;
+
+        if (he.getRequestURI().toString().contains("getInventory")) {
+            reVal = String.valueOf(WSDLClient.getWHInventory());
+        } else if (he.getRequestURI().toString().contains("pickSpecificTray")){
+            WSDLClient.pickSpecificTray(Integer.parseInt(he.getRequestURI().toString().split("\\?")[1]));
+            reVal = he.getRequestURI().toString().split("\\?")[1];
+        } else {
+            reVal = "Value is null";
         }
 
         httpResponse(he, reVal);
     }
 
-    private String handleGet(HttpExchange he) {
-        String getReturnValue = null;
-        switch (he.getRequestURI().toString()) {
-            case "http://localhost:8001/1/getInventory":
-                getReturnValue = String.valueOf(WSDLClient.getWHInventory());
-                break;
-            default:
-                getReturnValue = "Value is null";
-        }
-        System.out.println("Made it to get");
-        return getReturnValue;
-    }
-
-    private String handlePost(HttpExchange he) {
-        String postReturnValue = null;
-        switch (he.getRequestURI().toString()) { //Potentially the switch case is what is messing it up
-            case "http://localhost:8001/1/InsertItemInventory":
-                System.out.println(he.getRequestURI());
-                System.out.println("test");
-                postReturnValue = String.valueOf(ies.getBasicHttpBindingIEmulatorService().getInventory());
-                break;
-
-            case "localhost:8001/1/PickItemInventory":
-                System.out.println(he.getRequestBody().toString());
-                break;
-
-            default:
-                System.out.println("i got to default");
-                postReturnValue = "Value is null";
-        }
-        System.out.println("made it to post");
-        return postReturnValue;
-    }
-
     private void httpResponse(HttpExchange he, String reVal) throws IOException {
         OutputStream os = he.getResponseBody();
-        sb.append(reVal);
+        StringBuilder sb = new StringBuilder(reVal);
 
         String hr = StringEscapeUtils.escapeHtml4(sb.toString());
 
