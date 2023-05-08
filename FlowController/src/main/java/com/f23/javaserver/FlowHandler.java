d4df91e2dd44e9ed3042d02e75fe7461822909b7
Christoffer Rosenkilde <Crose19@student.sdu.dk>
2023-05-08 12:40:31 +0200

    Construction of the FlowServer and FlowHandler

---
+++ FlowController/src/main/java/com/f23/javaserver/FlowHandler.java
@@ -0,0 +1,13 @@
+package com.f23.javaserver;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class FlowHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+    }
+}
---
+++ FlowController/src/main/java/com/f23/javaserver/FlowServer.java
@@ -0,0 +1,29 @@
+package com.f23.javaserver;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class FlowServer {
+    public static void StartServer(ThreadPoolExecutor tpe) {
+        HttpServer hs;
+
+        try {
+            hs = HttpServer.create(new InetSocketAddress("localhost",8001),0);
+            hs.createContext("/1/", new FlowHandler());
+
+            hs.setExecutor(tpe);
+            hs.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void main(String[] args) {
+        ThreadPoolExecutor tpe = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+
+        FlowServer.StartServer(tpe);
+    }
+}
