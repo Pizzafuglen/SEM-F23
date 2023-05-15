e45097616b7e4307e901988427cc603658a78367
ChristofferReitz <chrei21@student.sdu.dk>
2023-05-15 13:21:30 +0200

    test v2

--- FrontEnd/scripts.js
+++ FrontEnd/scripts.js
@@ -12,7 +12,7 @@ var redirectButton = document.getElementById("redirectToHome");
 //add a click event listener to the button
 redirectButton.addEventListener('click', function(){
 //send the url and redirect
-var urlToSend = "http://127.0.0.1:5500/FrontEnd/recipe.html"
+var urlToSend = "http://127.0.0.1:5500/FrontEnd/home.html"
 redirectTo(urlToSend);
 });
 })
@@ -31,5 +31,58 @@ redirectTo(urlToSend);
 });
 })
 
+// js function for the stockinfo page
+// add DOMContentLoaded event listener to execute the code
+document.addEventListener('DOMContentLoaded',function(){
+    // get the button element
+var redirectButton = document.getElementById("redirectToStockInfo");
 
+//add a click event listener to the button
+redirectButton.addEventListener('click', function(){
+//send the url and redirect
+var urlToSend = "http://127.0.0.1:5500/FrontEnd/stockInfo.html"
+redirectTo(urlToSend);
+});
+})
 
+// js function for the recipe page
+// add DOMContentLoaded event listener to execute the code
+document.addEventListener('DOMContentLoaded',function(){
+    // get the button element
+var redirectButton = document.getElementById("redirectToRecipe");
+
+//add a click event listener to the button
+redirectButton.addEventListener('click', function(){
+//send the url and redirect
+var urlToSend = "http://127.0.0.1:5500/FrontEnd/recipe.html"
+redirectTo(urlToSend);
+});
+})
+
+// js function for the Stock info page
+// add DOMContentLoaded event listener to execute the code
+document.addEventListener('DOMContentLoaded',function(){
+    // get the button element
+var redirectButton = document.getElementById("redirectToProductionInfo");
+
+//add a click event listener to the button
+redirectButton.addEventListener('click', function(){
+//send the url and redirect
+var urlToSend = "http://127.0.0.1:5500/FrontEnd/productionInfo.html"
+redirectTo(urlToSend);
+});
+})
+
+// js function for the Stock info page
+// add DOMContentLoaded event listener to execute the code
+document.addEventListener('DOMContentLoaded',function(){
+    // get the button element
+var redirectButton = document.getElementById("redirectToFinished");
+
+//add a click event listener to the button
+redirectButton.addEventListener('click', function(){
+//send the url and redirect
+var urlToSend = "http://127.0.0.1:5500/FrontEnd/finished.html"
+redirectTo(urlToSend);
+});
+})
